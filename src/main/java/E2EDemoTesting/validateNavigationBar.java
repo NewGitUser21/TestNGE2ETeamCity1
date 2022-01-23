18fa3429f916675ad7ca2e01fab65f013b8a157f
Soubhagya Ranjan Jena <jena.soubhagya21@gmail.com>
2022-01-23 20:05:56 +0530

    first commit

---
+++ src/main/java/E2EDemoTesting/validateNavigationBar.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ * Adding logs using Log4j
+ * 
+ * log4j xml file
+ * 
+ * Generating reports
+ * 
+ * Screenshots on Failure
+ * 
+ * Jenkins Integration
+ * 
+ */
+package E2EDemoTesting;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ * @author DELL
+ * 
+ * Adding Tests to Suite in Framework guidelines
+ * 
+ * Shift+Ctrl+'O' - To remove unused imports
+ *
+ */
+public class validateNavigationBar extends base {
+
+	
+	//Mandatory for Log4j, declaration of Log Object declaration
+		public static Logger log = LogManager.getLogger(base.class.getName());
+		
+		public static WebDriver driver;
+	
+	@BeforeTest
+	public void initialize() throws IOException
+	{
+		driver = initializeDriver();
+		//log.info("Driver is initialized");
+		Reporter.log("Driver is initialized");
+		
+        String urlName = prop.getProperty("url");	
+		driver.get(urlName);
+		//log.info("Navigated to Home Page");
+		Reporter.log("Navigated to Home Page");
+		
+	}
+	
+	
+	//@Test(dataProvider="getData")
+	@Test
+	public static void basePageNavigation() throws IOException, InterruptedException
+	{ 
+		//HomePage HP = new HomePage();
+		//driver = initializeDriver();
+		
+		//Pass URL through driver object
+		
+		//Before adding url to global data properties file
+		//driver.get("https://qaclickacademy.com/");
+		
+		
+		//After adding url to global data properties file
+		//prop.getProperty("url")
+		
+		//String urlName = prop.getProperty("url");
+		
+		//driver.get(urlName);
+		
+		LandingPage l = new LandingPage(driver);
+		
+		//l.getNavigationBar().isDisplayed();
+		
+		
+		//Argument expect always to be True
+		Assert.assertTrue(l.getNavigationBar().isDisplayed());
+		
+		//log.info("Navigate Bar is displayed");
+		
+		Reporter.log("Navigate Bar is displayed");
+		
+			
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+		driver.close();
+		//driver.quit();
+		
+		Reporter.log("Driver closed");
+	}
+
+}
---
+++ src/main/java/E2EDemoTesting/validateTitle.java
@@ -0,0 +1,97 @@
+/**
+ * 
+ */
+package E2EDemoTesting;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ * @author DELL
+ * 
+ * Asserting test cases with validations
+ * 
+ * Shift+Ctrl+'O' - To remove unused imports
+ *
+ */
+public class validateTitle extends base {
+	
+	//Mandatory for Log4j, declaration of Log Object declaration
+	public static Logger log = LogManager.getLogger(base.class.getName());
+	
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void initialize() throws IOException
+	{
+		driver = initializeDriver();
+		//log.info("Driver is initialized");
+		Reporter.log("Driver is initialized");
+		
+        String urlName = prop.getProperty("url");	
+		driver.get(urlName);
+		
+		//log.info("Navigated to Home Page");
+		Reporter.log("Navigated to Home Page");
+		
+		
+	}
+	
+	
+	
+	//@Test(dataProvider="getData")
+	@Test
+	public static void basePageNavigation() throws IOException, InterruptedException
+	{ 
+		//HomePage HP = new HomePage();
+		//driver = initializeDriver();
+		//driver.get("https://qaclickacademy.com/");
+		
+		LandingPage l = new LandingPage(driver);
+		
+		//Compare the text with actual title text, if not then the script will fail
+		
+		//Assert.assertEquals(l.getTitle().getText(), "Featured Courses123");
+		
+		/*
+		 * Error Log FAILED: basePageNavigation java.lang.AssertionError: expected
+		 * [Featured Courses123] but found [FEATURED COURSES]
+		 */
+		
+		//Assert.assertEquals(l.getTitle().getText(), "Featured Courses");
+		
+		/*
+		 * Error Log FAILED: basePageNavigation java.lang.AssertionError: expected
+		 * [Featured Courses] but found [FEATURED COURSES]
+		 */
+		
+		//System.out.println(l.getTitle().getText());
+		
+		Assert.assertEquals(l.getTitle().getText(), "FEATURED COURSES123");
+		
+		//PASSED: basePageNavigation
+		
+		//log.info("Successfully validated text message");
+		Reporter.log("Successfully validated text message");
+		
+		
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+		driver.close();
+		//driver.quit();
+		
+		Reporter.log("Driver closed");
+	}
+
+}
